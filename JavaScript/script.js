569e5df49c30bcf8df10b8288d94e1a6eafc1f6f
wenchieh0119 <jamie1063021@gmail.com>
2023-12-17 23:50:33 +0800

    刪除

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -37,7 +37,7 @@ AOS.init({
 $(function() {
     var $win = $(window);
     var $backToTop = $('.js-back-to-top');
-    // 当用户滚动到离顶部100像素时，展示回到顶部按钮
+    //回最上面按钮
     $win.scroll(function() {
       if ($win.scrollTop() > 100) {
         $backToTop.show();
@@ -45,7 +45,7 @@ $(function() {
         $backToTop.hide();
       }
     });
-    // 当用户点击按钮时，通过动画效果返回头部
+  
     $backToTop.click(function() {
       $('html, body').animate({
         scrollTop: 0
@@ -57,7 +57,7 @@ $(function() {
 
 
 
-
+//換內容
   $(function(){
     var $li = $('ul.path li');
         $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.card').hide();
